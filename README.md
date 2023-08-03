94deb19f1053ecff1f1b677b15d5c3bf0541bc72
Cutler Sheridan <cutler.sheridan@gmail.com>
2023-08-03 13:55:33 -0700

    0.0.0 Initial commit; project setup

---
+++ README.md
@@ -0,0 +1,25 @@
+# Members Only
+
+## A message board with varying degrees of accessibility depending on if user is signed in, a club member, and/or an admin
+
+Post messages. If you're not signed in, you can view messages, but not who posted them. If you are signed in and a club member, you can see who posted them. If you're an admin, you can delete them, too.
+
+Made using Node, Express, MongoDB, Passport, bcryptjs, EJS, HTML, CSS.
+
+#### TODO NEXT
+
+#### TODO LATER
+
+##### Features
+
+##### Behavior
+
+##### Style
+
+- add credit
+
+#### DONE
+
+_0.0.0_
+
+- Initial commit; project setup
---
+++ configs/passport_config.js
@@ -0,0 +1,39 @@
+const LocalStrategy = require('passport-local').Strategy;
+const { db, ObjectId } = require('./mongodb_config');
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await db.collection('users').findOne({ username });
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        }
+        return done(null, false, { message: 'Incorrect password' });
+      });
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db
+      .collection('users')
+      .findOne({ _id: new ObjectId(id) });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+module.exports = passport;
